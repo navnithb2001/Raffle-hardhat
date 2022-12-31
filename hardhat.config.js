81eb830ba30918731b7623185f29143f3baf8066
navnithb2001 <bharadwajnavnith5@gmail.com>
2022-12-31 17:04:44 +0530

    init

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-deploy");
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 6
+    },
+  },
+  solidity: "0.8.17",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
+};
